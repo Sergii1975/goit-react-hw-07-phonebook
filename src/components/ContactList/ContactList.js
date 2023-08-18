fd188c7805786d76c777b74947366a1670b82a6e
Sergii <svbulgakov75@gmail.com>
2023-08-18 23:11:09 +0300

    updtFin

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,31 +1,16 @@
-import { useSelector } from 'react-redux';
-import { selectContactsFilter, selectContactsList } from 'redux/selectors';
+import { useSelector} from 'react-redux';
 import { ContactsListItem } from '../ContactListItem/ContactList.Item';
-
-// import { createSelector } from "@reduxjs/toolkit";
-
+import { selectVisibleContacts } from 'redux/selectors';
 import { ContactsList } from './ContactList.styled';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContactsList);
-  const filter = useSelector(selectContactsFilter);
-  const visibleContacts = [
-    ...contacts.filter(contact => contact.name.toLowerCase().includes(filter)),
-  ];
-
-//   export const selectVisibleContacts = createSelector(
-// [ selectContacts, selectFilter],
-//  (contacts, filter) => {
-//    return contacts.filter(contact => contact.name.toLowerCase()
-//     .includes(filter.toLowerCase()))
-//  }
-// )
-
-  console.log(visibleContacts);
-
+  const contacts = useSelector(selectVisibleContacts);
+ 
+  console.log(contacts);
+ 
   return (
     <ContactsList>
-      {visibleContacts.map(({ name, phone, id }) => (
+      {contacts.map(({ name, phone, id }) => (
         <ContactsListItem key={id} id={id} name={name} number={phone} />
       ))}
     </ContactsList>
